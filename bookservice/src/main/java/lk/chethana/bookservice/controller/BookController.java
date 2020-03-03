c3ba74dd6992c07a22196387d4b4a4c29aacfe58
Chethana <chethanad.jayarathne@gmail.com>
2020-03-03 22:58:06 +0530

    Change paths in book controller.

--- bookservice/src/main/java/lk/chethana/bookservice/controller/BookController.java
+++ bookservice/src/main/java/lk/chethana/bookservice/controller/BookController.java
@@ -9,52 +9,59 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/services")
+@RequestMapping("/book")
 public class BookController {
 
     @Autowired
     BookServiceImpl bookService;
 
-    @RequestMapping(value = "/addBook", method = RequestMethod.POST)
-    public Book addNewBook(@RequestBody Book book) {
+    @RequestMapping(method = RequestMethod.POST)
+    public Book addBook(@RequestBody Book book) {
 
         return bookService.addBook(book);
     }
 
-    @RequestMapping(value = "/getBook/{id}",method = RequestMethod.GET)
-    public Optional <Book> getBookById(@PathVariable Integer id){
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<Book> getBookById(@PathVariable Integer id) {
         return bookService.findById(id);
     }
 
-    @RequestMapping(value = "/updateBook/{id}",method = RequestMethod.PUT)
-    public Book updateBook(@PathVariable Integer id,@RequestBody Book bookDetails){
-         Optional <Book>book=bookService.findById(id);
-         Book updatedBook=null;
-         if(book.isPresent()){
-             Book newBook=book.get();
-             newBook.setIsbn(bookDetails.getIsbn());
-             newBook.setTitle(bookDetails.getTitle());
-             newBook.setSubject(bookDetails.getSubject());
-             newBook.setPublisher(bookDetails.getPublisher());
-             newBook.setLanguage(bookDetails.getLanguage());
-             newBook.setNoOfPages(bookDetails.getNoOfPages());
-             newBook.setAuthors(bookDetails.getAuthors());
-             updatedBook=bookService.addBook(newBook);
-         }else {
-             System.out.println("This book doesn't exist");
-         }
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Book updateBook(@PathVariable Integer id, @RequestBody Book bookDetails) {
+        Optional<Book> book = bookService.findById(id);
+        Book updatedBook = null;
+        if (book.isPresent()) {
+            Book newBook = book.get();
+            if (bookDetails.getIsbn() != null)
+                newBook.setIsbn(bookDetails.getIsbn());
+            if(bookDetails.getTitle()!=null)
+                newBook.setTitle(bookDetails.getTitle());
+            if(bookDetails.getSubject()!=null)
+                newBook.setSubject(bookDetails.getSubject());
+            if(bookDetails.getPublisher()!=null)
+                newBook.setPublisher(bookDetails.getPublisher());
+            if(bookDetails.getLanguage()!=null)
+                newBook.setLanguage(bookDetails.getLanguage());
+            if(bookDetails.getNoOfPages()!=null)
+                newBook.setNoOfPages(bookDetails.getNoOfPages());
+            if(bookDetails.getAuthors()!=null)
+                newBook.setAuthors(bookDetails.getAuthors());
+            updatedBook = bookService.addBook(newBook);
+        } else {
+            System.out.println("This book doesn't exist");
+        }
 
-         return updatedBook;
+        return updatedBook;
 
     }
 
-    @GetMapping(value = "/allBooks")
-    public List <Book> getAllBooks(){
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
-    @RequestMapping(value = "/deleteBook/{id}",method = RequestMethod.DELETE)
-    public void deleteBookById(@PathVariable Integer id){
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteBookById(@PathVariable Integer id) {
         bookService.deleteBook(id);
     }
 }
